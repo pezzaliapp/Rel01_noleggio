21c7d36af3d846ede4c6830c9a2874e255175eb8
pezzaliapp <pezzalialessandro@gmail.com>
2025-11-28 16:48:20 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# noleggio AGG. 28.07.2025
+Locazione Operativa di Noleggio BCC
---
+++ service-worker.js
@@ -0,0 +1,41 @@
+/* service-worker.js */
+const CACHE_NAME = "noleggio-cache-v1";
+const urlsToCache = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./app.js",
+    "./manifest.json",
+    "./icons/icons_crm-192x192.png",
+    "./icons/icons_crm-512x512.png"
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
